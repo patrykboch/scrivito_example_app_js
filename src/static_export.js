a62164a1cb743694f4bf30d4cf9e313850d267f8
Alexander Pepper <alexander.pepper@infopark.de>
2018-05-28 12:30:53 +0000

    Added a very first version of static export.

---
+++ src/static_export.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as Scrivito from 'scrivito';
+import './Objs';
+import './Widgets';
+import App from './App';
+import './config';
+
+ReactDOM.render(<App />, document.getElementById('application'));
