ced85c1f84517c365162fe789c3b1b007903500f
Alexander Pepper <alexander.pepper@infopark.de>
2018-05-31 10:23:50 +0200

    Reanimate: Preload website, before replacing it.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import './polyfills';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as ReactDOMServer from 'react-dom/server';
 import * as Scrivito from 'scrivito';
 import './Objs';
 import './Widgets';
 import App from './App';
 import './config';
 
-window.prerenderReady = false;
-ReactDOM.render(<App />, document.getElementById('application'));
-Scrivito.finishLoading().then(() => { window.prerenderReady = true; });
+const appElement = document.getElementById('application');
+const prerenderedObjId = appElement.getAttribute('data-scrivito-prerendering-obj-id');
+
+if (prerenderedObjId) {
+  Scrivito.load(
+    () => Scrivito.Obj.get(prerenderedObjId)
+  ).then(
+    obj => Scrivito.load(
+      () => Scrivito.withPage(
+        obj,
+        () => ReactDOMServer.renderToStaticMarkup(<App />)
+      )
+    )
+  ).then(
+    () => {
+      ReactDOM.render(<App />, appElement);
+    }
+  );
+} else {
+  window.prerenderReady = false;
+  ReactDOM.render(<App />, appElement);
+  Scrivito.finishLoading().then(() => { window.prerenderReady = true; });
+}
