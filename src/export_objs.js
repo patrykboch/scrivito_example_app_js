2471200bbd737571314eeef76b8e37f61d20576c
Alexander Pepper <alexander.pepper@infopark.de>
2018-05-31 09:15:55 +0000

    exportObjs also exports headContent, htmlAttributes and bodyAttributes.
    As provided by Helmet.renderStatic(). See https://github.com/nfl/react-helmet#server-usage for details.

--- src/export_objs.js
+++ src/export_objs.js
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import * as ReactDOMServer from 'react-dom/server';
 import * as Scrivito from 'scrivito';
+import Helmet from 'react-helmet';
 import './Objs';
 import './Widgets';
 import App from './App';
@@ -20,12 +21,25 @@ function allObjs() {
 }
 
 function exportObj(obj) {
+  // Tell helmet to pretend to run on a node server, not in a browser
+  // See https://github.com/nfl/react-helmet/issues/310 for details
+  Helmet.canUseDOM = false;
+
   return Scrivito.load(() =>
     Scrivito.withPage(obj, () => {
+      const bodyContent = ReactDOMServer.renderToStaticMarkup(<App />);
+      const helmet = Helmet.renderStatic();
       return {
         objId: obj.id(),
         objUrl: Scrivito.urlFor(obj),
-        bodyContent: ReactDOMServer.renderToStaticMarkup(<App />),
+        htmlAttributes: helmet.htmlAttributes.toString(),
+        headContent: `
+          ${helmet.title.toString()}
+          ${helmet.meta.toString()}
+          ${helmet.link.toString()}
+        `,
+        bodyAttributes: helmet.bodyAttributes.toString(),
+        bodyContent,
       };
     })
   );
