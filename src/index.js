2ec043a1998b3f0049648c9cd9302bf0238112ff
Alexander Pepper <alexander.pepper@infopark.de>
2018-08-27 15:06:51 +0200

    Reanimate uses new "Scrivito.preload" method.

--- src/index.js
+++ src/index.js
@@ -1,49 +1,19 @@
 import './polyfills';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as ReactDOMServer from 'react-dom/server';
 import * as Scrivito from 'scrivito';
 import './Objs';
 import './Widgets';
 import App from './App';
 import './config';
 
-function preloadHomepage() {
-  // TODO Load "homepage" callback of "Scrivito.configure", if configured
-  return Scrivito.Obj.root();
+let preloadDump = '';
+const preloadDumpElement = document.getElementById('preloadDump');
+if (preloadDumpElement) {
+  const preloadDumpData = preloadDumpElement.getAttribute('data-scrivito-preload-dump');
+  preloadDump = decodeURIComponent(preloadDumpData);
 }
 
-function preloadPermalink() {
-  // TODO remove "routingBasePath" of "Scrivito.configure" from path, if configured
-  const pathToRecognize = window.location.pathname;
-  const path = pathToRecognize.replace(new RegExp('^/+|/+$', 'g'), '');
-  return Scrivito.Obj.getByPermalink(path);
-}
-
-const appElement = document.getElementById('application');
-const prerenderedObjId = appElement.getAttribute('data-scrivito-prerendering-obj-id');
-
-if (prerenderedObjId) {
-  Scrivito.load(
-    () => Scrivito.Obj.get(prerenderedObjId)
-  ).then(
-    obj => Scrivito.load(
-      () => {
-        preloadHomepage();
-        preloadPermalink();
-        return Scrivito.renderPage(
-          obj,
-          () => ReactDOMServer.renderToStaticMarkup(<App />)
-        );
-      }
-    )
-  ).then(
-    () => {
-      ReactDOM.render(<App />, appElement);
-    }
-  );
-} else {
-  window.prerenderReady = false;
-  ReactDOM.render(<App />, appElement);
-  Scrivito.finishLoading().then(() => { window.prerenderReady = true; });
-}
+Scrivito.preload(preloadDump).then(() => {
+  ReactDOM.render(<App />, document.getElementById('application'));
+});
