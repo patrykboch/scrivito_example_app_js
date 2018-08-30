85d607d0722370a28f895742797379c387512d52
Alexander Pepper <alexander.pepper@infopark.de>
2018-08-30 10:20:05 +0000

    index.js: Re-added current page workarounds. They are still needed.

--- src/index.js
+++ src/index.js
@@ -7,12 +7,40 @@ import './Widgets';
 import App from './App';
 import './config';
 
-function renderApp() {
-  ReactDOM.render(<App />, document.getElementById('application'));
+function preloadHomepage() {
+  // TODO Load "homepage" callback of "Scrivito.configure", if configured
+  return Scrivito.Obj.root();
 }
 
-if (window.preloadDump) {
-  Scrivito.preload(window.preloadDump).then(renderApp);
+function preloadPermalink() {
+  // TODO remove "routingBasePath" of "Scrivito.configure" from path, if configured
+  const pathToRecognize = window.location.pathname;
+  const path = pathToRecognize.replace(new RegExp('^/+|/+$', 'g'), '');
+  return Scrivito.Obj.getByPermalink(path);
+}
+
+const appElement = document.getElementById('application');
+const prerenderedObjId = appElement.getAttribute('data-scrivito-prerendering-obj-id');
+
+function renderApp() {
+  ReactDOM.render(<App />, appElement);
+}
+
+if (prerenderedObjId && window.preloadDump) {
+  Scrivito.load(
+    () => Scrivito.Obj.get(prerenderedObjId)
+  ).then(
+    () => Scrivito.load(
+      () => {
+        preloadHomepage();
+        preloadPermalink();
+      }
+    )
+  ).then(
+    () => Scrivito.preload(window.preloadDump).then(renderApp)
+  );
 } else {
+  window.prerenderReady = false;
   renderApp();
+  Scrivito.finishLoading().then(() => { window.prerenderReady = true; });
 }
