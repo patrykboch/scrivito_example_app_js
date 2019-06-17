55e15630e7ed6c89c45a26bd55b22913df2b96bc
patrykboch <patryk.bochenek@infopark.de>
2019-06-17 12:20:39 +0200

    Sample validations for testing

--- src/Widgets/TextWidget/TextWidgetClass.js
+++ src/Widgets/TextWidget/TextWidgetClass.js
@@ -3,8 +3,25 @@ import { registerTextExtract } from "../../utils/textExtractRegistry";
 
 const TextWidget = Scrivito.provideWidgetClass("TextWidget", {
   attributes: {
-    text: "html",
-    alignment: ["enum", { values: ["left", "center", "right"] }],
+    binary: "binary",
+    date: "date",
+    enum: ["enum", { values: ["left", "center", "right"] }],
+    link: "link",
+    linklist: "linklist",
+    multienum: [
+      "multienum",
+      {
+        values: ["FIRST", "SECOND", "THIRD", "FOURTH"],
+      },
+    ],
+    reference: "reference",
+    referencelist: "referencelist",
+    string: "string",
+    stringlist: "stringlist",
+    float: "float",
+    html: "html",
+    integer: "integer",
+    widgetlist: "widgetlist",
   },
 });
 
--- src/Widgets/TextWidget/TextWidgetEditingConfig.js
+++ src/Widgets/TextWidget/TextWidgetEditingConfig.js
@@ -5,22 +5,154 @@ Scrivito.provideEditingConfig("TextWidget", {
   title: "Text",
   thumbnail: textWidgetIcon,
   attributes: {
-    alignment: {
-      title: "Alignment",
-      description: "Default: Left",
-      values: [
-        { value: "left", title: "Left" },
-        { value: "center", title: "Center" },
-        { value: "right", title: "Right" },
-      ],
+    binary: {
+      title: "Binary",
     },
-    text: {
-      title: "Text",
-      description: "The actual source code of this text",
+    date: {
+      title: "Date",
+    },
+    enum: {
+      title: "Enum",
+    },
+    link: {
+      title: "Link",
+    },
+    linklist: {
+      title: "Linklist",
+    },
+    multienum: {
+      title: "multienum",
+    },
+    reference: {
+      title: "Reference",
+    },
+    referencelist: {
+      title: "Referencelist",
+    },
+    string: {
+      title: "String",
+    },
+    stringlist: {
+      title: "Stringlist",
+    },
+    float: {
+      title: "Float",
+    },
+    html: {
+      title: "HTML",
+    },
+    integer: {
+      title: "integer",
+    },
+    widgetlist: {
+      title: "Widgetlist",
     },
   },
-  properties: ["alignment", "text"],
-  initialContent: {
-    alignment: "left",
-  },
+  properties: [
+    "binary",
+    "date",
+    "enum",
+    "link",
+    "linklist",
+    "multienum",
+    "reference",
+    "referencelist",
+    "string",
+    "stringlist",
+    "float",
+    "html",
+    "integer",
+    "widgetlist",
+  ],
+  validations: [
+    [
+      "binary",
+      binary => {
+        if (binary === null) {
+          return "Binary Error!";
+        }
+      },
+    ],
+    [
+      "date",
+      date => {
+        if (date !== null) {
+          return "Date attribute error! I'm the second line and description of this error";
+        }
+      },
+    ],
+    [
+      "enum",
+      enumm => {
+        return "Enum attribute error!";
+      },
+    ],
+
+    [
+      "link",
+      link => {
+        return "link attribute error";
+      },
+    ],
+    [
+      "linklist",
+      linklist => {
+        return "linklist attribute error";
+      },
+    ],
+    [
+      "multienum",
+      multienum => {
+        return "Multienum attribute error";
+      },
+    ],
+    [
+      "reference",
+      reference => {
+        return "Reference attribute error";
+      },
+    ],
+    [
+      "referencelist",
+      referencelist => {
+        return "Referencelist attribute error";
+      },
+    ],
+    [
+      "string",
+      string => {
+        return "String attribute error";
+      },
+    ],
+    [
+      "stringlist",
+      stringlist => {
+        return "Stringlist attribute error";
+      },
+    ],
+    [
+      "float",
+      float => {
+        return "Float attribute error";
+      },
+    ],
+    [
+      "html",
+      html => {
+        return "HTML attribute error";
+      },
+    ],
+    [
+      "integer",
+      integer => {
+        return "Integer attribute error";
+      },
+    ],
+    [
+      "widgetlist",
+      widgetlist => {
+        return "Widgetlist attribute error";
+      },
+    ],
+  ],
 });
